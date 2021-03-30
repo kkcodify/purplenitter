72d8fbe7a79b7597683c1da0e20a19b4c5405df0
Varulv <varulv1997@yahoo.com>
2021-03-30 14:28:48 +0100

    Update config.nim

--- src/config.nim
+++ src/config.nim
@@ -29,7 +29,6 @@ proc getConfig*(path: string): (Config, parseCfg.Config) =
 
     redisHost: cfg.get("Cache", "redisHost", "localhost"),
     redisPort: cfg.get("Cache", "redisPort", 6379),
-    redisAuth: cfg.get("Cache", "redisAuth", ""),
     redisConns: cfg.get("Cache", "redisConnections", 20),
     redisMaxConns: cfg.get("Cache", "redisMaxConnections", 30)
   )
